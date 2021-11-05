efbf2c74c7c5799a168ccede96e28598a3b9aac8
IshitaNarsiker <ishitanarsiker@gmail.com>
2021-11-05 09:13:11 +0000

    Create Flashcard.js

---
+++ src/Flashcard.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+export default function Flashcard({ flashcard }) {
+  const [flip, setFlip] = useState(false)
+  const [height, setHeight] = useState('initial')
+
+  const frontEl = useRef()
+  const backEl = useRef()
+
+  function setMaxHeight() {
+    const frontHeight = frontEl.current.getBoundingClientRect().height
+    const backHeight = backEl.current.getBoundingClientRect().height
+    setHeight(Math.max(frontHeight, backHeight, 100))
+  }
